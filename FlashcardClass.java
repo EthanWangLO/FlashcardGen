fd6d09f0f9172f77475b87eaf13eab884bb50801
EthanWangLO <26wange@smtexas.org>
2025-05-14 12:25:21 -0500

    Create FlashcardClass.java

---
+++ FlashcardClass.java
@@ -0,0 +1,14 @@
+public class Flashcard{
+  private String front;
+  private String back;
+
+  public Flashcard(String fr, String ba)
+  {
+    this.front = fr;
+    this.back = ba;
+  }
+  public String getFront()
+  { return front;}
+  public String getBack()
+  { return back;}
+}
